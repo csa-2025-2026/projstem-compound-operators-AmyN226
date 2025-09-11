01a632241cdfb5cb2c4b61448ad6c81fcb2c30b5
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-09-11 02:28:10 +0000

    finished

--- Problem2.java
+++ Problem2.java
@@ -1,4 +1,14 @@
 public class Problem2
 {
-    
+    public static void main(String[] args)
+    {
+        int x = 876;
+        
+        
+        System.out.println((x / 100));
+
+        System.out.println((x % 100) / 10);
+
+        System.out.println(x % 10);
+    }    
 }
